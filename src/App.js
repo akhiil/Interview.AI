2fa0647adc0042e6814dc64292710a0ec27b8be1
akhil <akhilanand@pbpartners.com>
2023-02-04 15:36:06 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainContainer" />
+    <div className="">
+        <h2>Welcome to AI interview</h2>``
+        <h2>Blurred Background</h2>
+        <h1>I am John Doe</h1>
+        <p>And I'm a Photographer</p>
+      </div>
     </div>
   );
 }
